a5852c790f24b4ad7ee6441b5f0959a71561ae62
Taiga TAKANO <53041471+TakanoTaiga@users.noreply.github.com>
2023-03-12 22:17:36 +0900

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
+function print_error {
+    tput setaf 1
+    echo "$1"
+    tput sgr0
+}
+
+function print_warning {
+    tput setaf 3
+    echo "$1"
+    tput sgr0
+}
+
+function print_info {
+    tput setaf 2
+    echo "$1"
+    tput sgr0
+}
+
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $ROOT/util/print_util.sh
 
 
-# IMAGE_NAME="robo2022:latest"
-# print_warning "Launch image: ${IMAGE_NAME} with base: ${LAUCNHFILE_DIR}"
-# read -rp ">  Are you sure you want to run setup? [y/N] " answer
-# if ! [[ ${answer:0:1} =~ y|Y ]]; then
-#     echo -e "\e[33mCancelled.\e[0m"
-#     exit 1
-# fi
+# Check Arg
+if [ $# -eq 0 ]; then
+    print_error "Not found image name."
+    exit 1
+fi
+
+if [[ ! $1 =~ ":" ]]; then
+    print_error "Not found tag name."
+    exit 1
+fi
 
 
 docker run --rm -it \
     --privileged \
     --network host \
-    -v $ROOT/../adaos_workspace/launch:/adaos_workspace/launch \
+    -v $ROOT/../launch:/adaos_workspace/launch \
     -v /dev/*:/dev/* \
     --workdir /adaos_workspace/ \
     --entrypoint /usr/local/bin/scripts/launch-entrypoint.sh \
-    sc3_gw:latest \
+    $1 \
     /bin/bash
